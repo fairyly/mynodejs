b5cd9f754bc0c4f9f995ac1d520a1c5afd68cc1b
fairyly <fairyilys@gmail.com>
2018-06-09 14:52:51 +0800

    add study node

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ mynodejs
 ```
 
 
+### 01 
+
+Node：单线程
+
+事件轮询： Node 先去注册事件，随后不停的询问内核这些事件是否分发，当事件分发时，
+  对应的回调函数就会触发，然后继续执行下去，如果没有事件触发，则继续执行其他代码，直到有新事件时，再去执行对应的回调函数
+- get colors in your node.js console :在 node 控制台打印颜色
+  - colors: https://github.com/Marak/colors.js
+
+'\033[33mEnter your choice: \033[339m': (ANSI转义码)让文本呈现颜色
+
+用户输入： process.stdout.write()
+监听用户的输入: process.stdin.on('data', function (chunk) {})
+获取当前工作目录： process.cwd();
+
+
+- TCP：面向连接的
+  - 面向字节
+  - 可靠性
+  - 流控制
+  - 拥堵控制
+  - 
---
+++ node01/index.js
@@ -0,0 +1,83 @@
+require('colors');
+console.log('smashing node'.rainbow);
+
+// http 
+var http = require('http');
+http.createServer(function(req,res){
+	var buf = '';
+	req.on('data',function(data){
+		bud += data;
+		console.log(data)
+	})
+
+	req.on('end',function(){
+		console.log('接收完毕')
+	})
+	res.writeHead(200,{});
+	res.end('hello');
+	// throw new Error('错误不会被捕捉');
+
+
+}).listen(3000)
+
+// console.log('server is on http://127.0.0.1:3000',global)
+console.log("==============================================")
+// console.log(process)
+
+console.log(__filename)
+console.log(__dirname)
+
+// 添加 uncaughtException 处理器
+process.on('uncaughtException',function(err){
+	console.error(err);
+	process.exit(1);
+})
+// process.stdout.write('\033[33mEnter your choice: \033[339m')
+// process.stdin.resume()
+process.stdin.setEncoding('utf8');
+
+// 输入参数
+var num1, num2;
+/*2：向屏幕输出，提示信息，要求输入num1*/
+process.stdout.write('请输入num1的值：');
+/*3：监听用户的输入*/
+process.stdin.on('data', function (chunk) {
+    if (!num1) {
+        num1 = Number(chunk);
+        /*4：向屏幕输出，提示信息，要求输入num2*/
+        process.stdout.write('请输入num2的值');
+    } else {
+        num2 = Number(chunk);
+        process.stdout.write('结果是：' + (num1 + num2));
+    }
+});
+
+// net
+var net = require('net');
+net.createServer(function(con){
+	con.on('error',function(err){
+		console.log(err)
+	})
+}).listen(4000)
+
+//EventEmitter
+
+var EventEmitter = require('events').EventEmitter
+var a = new EventEmitter;
+a.on('event',function(res){
+	console.log('res')
+})
+// a.emit('event')
+
+// buffer
+var mybuffer = new Buffer('==ii1j2i3hli23h','base64')
+// console.log(mybuffer)
+
+
+// fs
+var fs = require('fs');
+fs.writeFile('logo.png',mybuffer,function(err){
+	if(err){console.log(err)}
+	// console.log("save")
+})
+
