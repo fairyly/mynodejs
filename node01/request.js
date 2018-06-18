7db5ebeaf1b7d7a2cd2215381cf46e809f6c1ea4
fairyly <fairyilys@gmail.com>
2018-06-18 15:21:17 +0800

    add cnode api test

--- node01/request.js
+++ node01/request.js
@@ -13,36 +13,51 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
 app.get('/', function (req, res, next) {
-  // superagent.get('https://cnodejs.org/api/v1/topics')
-  //   .send({
-  //     "page": 1,
-  //     "tab": 'share' ,
-  //     "limit": 20,
-  //     "mdrender": true
-  //   })
-  //   .end(function (err, sres) {
-  //     if (err) {
-  //       return next(err);
-  //     }
-  //     console.log(sres.body.data)
-  //     res.render('index',{data:sres.body.data});
-  //   });
-  axios.get('https://cnodejs.org/api/v1/topics', {
-    params: {
+  superagent.get('https://cnodejs.org/api/v1/topics')
+    .send({
       "page": 1,
       "tab": 'share' ,
       "limit": 20,
       "mdrender": true
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-    res.render('index',{data:response.data.data});
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    })
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+      console.log(sres.body.data)
+      res.render('index',{data:sres.body.data});
+    });
+  // axios.get('https://cnodejs.org/api/v1/topics', {
+  //   params: {
+  //     "page": 1,
+  //     "tab": 'share' ,
+  //     "limit": 20,
+  //     "mdrender": true
+  //   }
+  // })
+  // .then(function (response) {
+  //   console.log(response.data.data);
+  //   // res.render('index',{data:response.data.data});
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
+  // res.render('index',{data:[]});
 });
+
+app.get('/topic/:id', function (req, res, next) {
+console.log(req.params.id)
+  superagent.get('https://cnodejs.org/api/v1/topic/'+req.params.id)
+
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+      console.log(sres.body.data)
+      res.render('topic',{topic:sres.body.data});
+    });
+
+})
 
 
 app.listen(3000, function () {
