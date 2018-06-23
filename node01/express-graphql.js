bbd8e63a046c5ec620831c991ea779ac2944f603
fairyly <fairyilys@gmail.com>
2018-06-23 16:54:03 +0800

    add graphql test

---
+++ node01/express-graphql.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
---
+++ node01/graphql.js
@@ -0,0 +1,13 @@
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
